1b21690a7686b92f8fbb14ff78878bf072084f1b
phoBowl <tdnam39@gmail.com>
2017-07-13 23:27:42 +1000

    Create sol.java

---
+++ 30 Days of Code/Day 19/sol.java
@@ -0,0 +1,14 @@
+//Write your code here
+class Calculator implements AdvancedArithmetic {
+    private int n;
+    
+    public int divisorSum(int n){
+        int sum =0;
+        for(int i =1 ; i <= n; i++){
+            if(n%i == 0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
